d3fbdfce72c3120191053b49c41c23b3119c7e06
Niki Burgos <burgosveganicole@gmail.com>
2025-09-25 18:29:18 -0300

    agregado projecto dominio y clases segun el schema de la db

---
+++ dominio/Articulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace dominio
+{
+    public class Articulo //lo pongo public para que pueda ser accedido desde la capa de negocio (o sea desde otros proyectos)
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
+        public decimal Precio { get; set; }
+        public List<Imagen> Imagen { get; set; }
+
+    }
+}
