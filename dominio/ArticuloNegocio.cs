d7a458de7727ebe81e602def39350f2e504279d3
Guillermo <guillermocampos566@gmail.com>
2025-09-28 17:55:22 -0300

    Listado con tablas relacionadas

--- dominio/ArticuloNegocio.cs
+++ dominio/ArticuloNegocio.cs
@@ -20,12 +20,18 @@ namespace dominio
             {
                 conexion.ConnectionString = "Server=localhost,1433;Database=CATALOGO_P3_DB;User Id=sa;Password=Doc39805119;";
                 comando.CommandType = System.Data.CommandType.Text;
-                comando.CommandText = "select id, codigo, Nombre, Descripcion, idMarca, Precio from ARTICULOS";
+                comando.CommandText = @"SELECT a.id, a.codigo, a.Nombre, a.Descripcion,
+                                        a.idMarca, m.Descripcion AS MarcaDescripcion, 
+                                        a.idCategoria, c.Descripcion AS CategoriaDescripcion, 
+                                        a.Precio
+                                       FROM ARTICULOS a
+                                       INNER JOIN MARCAS m ON a.idMarca = m.Id
+                                       INNER JOIN CATEGORIAS c ON a.idCategoria = c.Id";
                 comando.Connection = conexion;
 
                 conexion.Open();
                 lector = comando.ExecuteReader();
-                
+
                 while (lector.Read())
                 {
                     Articulo aux = new Articulo();
@@ -33,8 +39,17 @@ namespace dominio
                     aux.Codigo = (string)lector["Codigo"];
                     aux.Nombre = (string)lector["Nombre"];
                     aux.Descripcion = (string)lector["Descripcion"];
-                    //aux.Marca = 
+
+                    aux.Marca = new Marca();
+                    aux.Marca.Id = (int)lector["idMarca"];
+                    aux.Marca.Descripcion = (string)lector["MarcaDescripcion"];
+
+                    aux.Categoria = new Categoria();
+                    aux.Categoria.Id = (int)lector["idCategoria"];
+                    aux.Categoria.Descripcion = (string)lector["CategoriaDescripcion"];
+
                     aux.Precio = (decimal)lector["Precio"];
+
 
                     lista.Add(aux);
                 }
