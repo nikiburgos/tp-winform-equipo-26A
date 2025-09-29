b72bbcc322679644ccc897fef6460b1ea381be39
Niki Burgos <burgosveganicole@gmail.com>
2025-09-29 17:26:19 -0300

    agregado projecto NEGOCIO, modificado el form de listado de articulos y creado accesodatos.cs para conexion con db

---
+++ negocio/AccesoDatos.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace negocio
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        // aca lo que hago es crear diferentes métodos. 
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+
+        }
+
+        //Constructor
+        public AccesoDatos()
+        //1: server, el nombre de la instancia de la db: 2: nombre de la base de datos: 3: forma de conectarse
+        { 
+            conexion = new SqlConnection("server=(localdb)\\MSSQLLocalDB; database=CATALOGO_P3_DB; integrated security=true");
+            comando = new SqlCommand();
+        }
+
+        public void setearConsulta(string consulta)
+        {
+            //comando realiza la acción e inyecta la consulta SQL de tipo texto .
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void ejecutarLectura()
+        {
+            //ejecuta el comando (consulta SQL (SELECT)) en la conexion (BD)
+            comando.Connection = conexion;
+            try
+            {
+                //se abre la conexion y ejecuta la lectura. Minuto 1h 42s de la clase 5 del profe Maxi. 
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                //ejecuta una acción que no sea de lectura (insert, delete, update)
+                comando.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void cerrarConexion()
+        {
+            //En caso de haber un lector activo cuando se llama al método, también se cierra junto con la conexión.
+            if (lector != null)
+                lector.Close();
+            conexion.Close();
+        }
+    }
+}
---
+++ negocio/ArticuloNegocio.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using dominio;
+
+namespace negocio
+{
+    public class ArticuloNegocio
+    {
+        public List<Articulo> listar()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta(@"SELECT a.id AS ArticuloId, a.codigo, a.Nombre, a.Descripcion, 
+                                        a.idMarca, m.Descripcion AS MarcaDescripcion, 
+                                        a.idCategoria, c.Descripcion AS CategoriaDescripcion, 
+                                        a.Precio
+                                        FROM ARTICULOS a
+                                        INNER JOIN MARCAS m ON a.idMarca = m.Id
+                                        INNER JOIN CATEGORIAS c ON a.idCategoria = c.Id
+                                        ORDER BY a.id");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    aux.Id = (int)datos.Lector["ArticuloId"];
+                    aux.Codigo = (string)datos.Lector["Codigo"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+
+                    aux.Marca = new Marca();
+                    aux.Marca.Id = (int)datos.Lector["idMarca"];
+                    aux.Marca.Descripcion = (string)datos.Lector["MarcaDescripcion"];
+
+                    aux.Categoria = new Categoria();
+                    aux.Categoria.Id = (int)datos.Lector["idCategoria"];
+                    aux.Categoria.Descripcion = (string)datos.Lector["CategoriaDescripcion"];
+
+                    aux.Precio = (decimal)datos.Lector["Precio"];
+
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
